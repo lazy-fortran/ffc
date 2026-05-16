02dbba424a2b9c3aba1209e11ef995a2263cc0c8
Christopher Albert <albert@tugraz.at>
2026-05-16 17:29:29 +0200

    fix: reject unsupported external scalar calls

--- test_mvp/test_session_unsupported_diagnostics.f90
+++ test_mvp/test_session_unsupported_diagnostics.f90
@@ -18,6 +18,7 @@ program test_session_unsupported_diagnostics
     if (.not. test_cycle_diagnostic()) all_passed = .false.
     if (.not. test_select_case_diagnostic()) all_passed = .false.
     if (.not. test_unsupported_intrinsic_diagnostic()) all_passed = .false.
+    if (.not. test_external_function_call_diagnostic()) all_passed = .false.
     if (.not. test_cli_array_declaration_diagnostic()) all_passed = .false.
     if (.not. test_cli_character_expression_diagnostic()) all_passed = .false.
     if (.not. test_cli_module_diagnostic()) all_passed = .false.
@@ -26,6 +27,7 @@ program test_session_unsupported_diagnostics
     if (.not. test_cli_cycle_diagnostic()) all_passed = .false.
     if (.not. test_cli_select_case_diagnostic()) all_passed = .false.
     if (.not. test_cli_unsupported_intrinsic_diagnostic()) all_passed = .false.
+    if (.not. test_cli_external_function_call_diagnostic()) all_passed = .false.
 
     if (.not. all_passed) stop 1
     print *, 'PASS: unsupported direct-session features emit diagnostics'
@@ -141,6 +143,18 @@ contains
                                            '/tmp/ffc_session_intrinsic_diagnostic_test')
     end function test_unsupported_intrinsic_diagnostic
 
+    logical function test_external_function_call_diagnostic()
+        character(len=*), parameter :: source = &
+                                       'program main'//new_line('a')// &
+                                       '  integer :: x'//new_line('a')// &
+                                       '  x = external_value(1)'//new_line('a')// &
+                                       'end program main'
+
+        test_external_function_call_diagnostic = expect_error_contains( &
+                                     source, 'unsupported scalar function call', &
+                                     '/tmp/ffc_session_external_call_test')
+    end function test_external_function_call_diagnostic
+
     logical function test_cli_array_declaration_diagnostic()
         character(len=*), parameter :: source = &
                                        'program main'//new_line('a')// &
@@ -249,6 +263,18 @@ contains
                                          source, 'unsupported scalar intrinsic: sqrt', &
                                                '/tmp/ffc_cli_intrinsic_diagnostic_test')
     end function test_cli_unsupported_intrinsic_diagnostic
+
+    logical function test_cli_external_function_call_diagnostic()
+        character(len=*), parameter :: source = &
+                                       'program main'//new_line('a')// &
+                                       '  integer :: x'//new_line('a')// &
+                                       '  x = external_value(1)'//new_line('a')// &
+                                       'end program main'
+
+        test_cli_external_function_call_diagnostic = expect_cli_error_contains( &
+                                     source, 'unsupported scalar function call', &
+                                     '/tmp/ffc_cli_external_call_test')
+    end function test_cli_external_function_call_diagnostic
 
     logical function expect_error_contains(source, expected, exe_path)
         character(len=*), intent(in) :: source
