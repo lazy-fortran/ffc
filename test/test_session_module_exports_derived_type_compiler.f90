9debd37cc1bef05856db8a90cd9ee9b92d596fe2
Christopher Albert <albert@tugraz.at>
2026-05-20 06:11:45 +0200

    refactor: migrate multi-test files to ffc_test_support (ref #216)

--- test/test_session_module_exports_derived_type_compiler.f90
+++ test/test_session_module_exports_derived_type_compiler.f90
@@ -1,8 +1,5 @@
 program test_session_module_exports_derived_type_compiler
-    use fortfront, only: compiler_frontend_options_t, &
-                         compiler_frontend_result_t, &
-                         compile_frontend_from_string, INPUT_MODE_STANDARD
-    use session_program_lowering, only: lower_program_to_liric_exe
+    use ffc_test_support, only: expect_exit_status, expect_output
     implicit none
 
     print *, '=== direct session module exports derived type compiler test ==='
@@ -32,8 +29,9 @@ contains
            '  stop 0'//new_line('a')// &
            'end program main'
 
-        test_module_with_single_derived_type = expect_compile(source, &
-            '/tmp/ffc_module_exports_single_test')
+        test_module_with_single_derived_type = expect_exit_status( &
+           source, 0, &
+           '/tmp/ffc_module_exports_single_test')
     end function test_module_with_single_derived_type
 
     logical function test_module_with_multiple_derived_types()
@@ -51,8 +49,9 @@ contains
            '  stop 0'//new_line('a')// &
            'end program main'
 
-        test_module_with_multiple_derived_types = expect_compile(source, &
-            '/tmp/ffc_module_exports_multi_test')
+        test_module_with_multiple_derived_types = expect_exit_status( &
+           source, 0, &
+           '/tmp/ffc_module_exports_multi_test')
     end function test_module_with_multiple_derived_types
 
     logical function test_empty_module_compiles()
@@ -63,8 +62,9 @@ contains
            '  stop 0'//new_line('a')// &
            'end program main'
 
-        test_empty_module_compiles = expect_compile(source, &
-            '/tmp/ffc_module_exports_empty_test')
+        test_empty_module_compiles = expect_exit_status( &
+           source, 0, &
+           '/tmp/ffc_module_exports_empty_test')
     end function test_empty_module_compiles
 
     logical function test_module_derived_type_with_component_assignment()
@@ -84,7 +84,7 @@ contains
            'end program main'
 
         test_module_derived_type_with_component_assignment = expect_output( &
-           source, '6', '/tmp/ffc_module_exports_component_test')
+           source, '6'//new_line('a'), '/tmp/ffc_module_exports_component_test')
     end function test_module_derived_type_with_component_assignment
 
     logical function test_module_with_many_derived_types()
@@ -143,8 +143,9 @@ contains
            '  stop 0'//new_line('a')// &
            'end program main'
 
-        test_module_with_many_derived_types = expect_compile(source, &
-            '/tmp/ffc_module_exports_many_test')
+        test_module_with_many_derived_types = expect_exit_status( &
+           source, 0, &
+           '/tmp/ffc_module_exports_many_test')
     end function test_module_with_many_derived_types
 
     logical function test_two_modules_with_derived_types()
@@ -163,8 +164,9 @@ contains
            '  stop 0'//new_line('a')// &
            'end program main'
 
-        test_two_modules_with_derived_types = expect_compile(source, &
-            '/tmp/ffc_module_exports_two_mods_test')
+        test_two_modules_with_derived_types = expect_exit_status( &
+           source, 0, &
+           '/tmp/ffc_module_exports_two_mods_test')
     end function test_two_modules_with_derived_types
 
     logical function test_module_with_only_integer_params()
@@ -176,8 +178,9 @@ contains
            '  stop 0'//new_line('a')// &
            'end program main'
 
-        test_module_with_only_integer_params = expect_compile(source, &
-            '/tmp/ffc_module_exports_params_test')
+        test_module_with_only_integer_params = expect_exit_status( &
+           source, 0, &
+           '/tmp/ffc_module_exports_params_test')
     end function test_module_with_only_integer_params
 
     logical function test_derived_type_with_single_component()
@@ -195,7 +198,7 @@ contains
            'end program main'
 
         test_derived_type_with_single_component = expect_output( &
-           source, '99', '/tmp/ffc_module_exports_single_comp_test')
+           source, '99'//new_line('a'), '/tmp/ffc_module_exports_single_comp_test')
     end function test_derived_type_with_single_component
 
     logical function test_derived_type_with_many_components()
@@ -223,95 +226,7 @@ contains
            'end program main'
 
         test_derived_type_with_many_components = expect_output( &
-           source, '55', '/tmp/ffc_module_exports_many_comp_test')
+           source, '55'//new_line('a'), '/tmp/ffc_module_exports_many_comp_test')
     end function test_derived_type_with_many_components
-
-    logical function expect_compile(source, exe_path)
-        character(len=*), intent(in) :: source
-        character(len=*), intent(in) :: exe_path
-        character(len=:), allocatable :: error_msg
-
-        expect_compile = .false.
-        call compile_and_lower(source, exe_path, error_msg)
-        call execute_command_line('rm -f '//exe_path)
-
-        if (len_trim(error_msg) > 0) then
-            print *, 'FAIL: lowering failed: ', trim(error_msg)
-            return
-        end if
-
-        expect_compile = .true.
-    end function expect_compile
-
-    logical function expect_output(source, expected, exe_path)
-        character(len=*), intent(in) :: source
-        character(len=*), intent(in) :: expected
-        character(len=*), intent(in) :: exe_path
-        character(len=:), allocatable :: error_msg
-        character(len=:), allocatable :: out_path
-        character(len=64) :: output_line
-        integer :: exit_stat
-        integer :: io_stat
-        integer :: unit
-
-        expect_output = .false.
-        out_path = exe_path//'.out'
-        call execute_command_line('rm -f '//exe_path//' '//out_path)
-        call compile_and_lower(source, exe_path, error_msg)
-        if (len_trim(error_msg) > 0) then
-            print *, 'FAIL: lowering failed: ', trim(error_msg)
-            call execute_command_line('rm -f '//exe_path//' '//out_path)
-            return
-        end if
-
-        call execute_command_line(exe_path//' > '//out_path, exitstat=exit_stat)
-        if (exit_stat /= 0) then
-            print *, 'FAIL: executable exit status ', exit_stat
-            call execute_command_line('rm -f '//exe_path//' '//out_path)
-            return
-        end if
-
-        open (newunit=unit, file=out_path, status='old', action='read', &
-              iostat=io_stat)
-        if (io_stat /= 0) then
-            print *, 'FAIL: could not open captured output'
-            call execute_command_line('rm -f '//exe_path//' '//out_path)
-            return
-        end if
-        read (unit, '(A)', iostat=io_stat) output_line
-        close (unit)
-        call execute_command_line('rm -f '//exe_path//' '//out_path)
-
-        if (io_stat /= 0 .or. trim(adjustl(output_line)) /= expected) then
-            print *, 'FAIL: expected output "', expected, '" got "', &
-                trim(output_line), '"'
-            return
-        end if
-
-        expect_output = .true.
-    end function expect_output
-
-    subroutine compile_and_lower(source, exe_path, error_msg)
-        character(len=*), intent(in) :: source
-        character(len=*), intent(in) :: exe_path
-        character(len=:), allocatable, intent(out) :: error_msg
-        type(compiler_frontend_options_t) :: options
-        type(compiler_frontend_result_t) :: frontend_result
-
-        options = compiler_frontend_options_t()
-        options%run_semantics = .true.
-        options%input_mode = INPUT_MODE_STANDARD
-
-        call compile_frontend_from_string(source, frontend_result, options)
-        if (.not. frontend_result%success()) then
-            error_msg = 'FortFront rejected source: '// &
-                        trim(frontend_result%diagnostic_text)
-            return
-        end if
-
-        call lower_program_to_liric_exe(frontend_result%arena, &
-                                        frontend_result%root_index, exe_path, &
-                                        error_msg)
-    end subroutine compile_and_lower
 
 end program test_session_module_exports_derived_type_compiler
