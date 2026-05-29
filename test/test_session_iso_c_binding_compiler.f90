98b6a33f955e933c1b286f27de65441c6e29cfcc
Christopher Albert <albert@tugraz.at>
2026-05-29 10:37:43 +0200

    feat: recognise iso_c_binding kind constants in declarations (#134)
    type_name_value_kind now splits a kinded type spec (integer(kind)) into
    its base type and kind, mapping default/4/c_int/c_int32_t/c_bool/c_char
    to i32, real/double precision/c_double to f64, and logical to logical.
    use iso_c_binding was already a no-op, so integer(c_int32_t) and friends
    now lower; numeric kinds like integer(4)/real(8) work too. 64-bit integer
    kinds (c_int64_t) are still rejected with a targeted diagnostic.
    
    Add test_session_iso_c_binding_compiler. c_ptr/c_null_ptr types are #135.

---
+++ test/test_session_iso_c_binding_compiler.f90
@@ -0,0 +1,58 @@
+program test_session_iso_c_binding_compiler
+    use ffc_test_support, only: expect_exit_status
+    implicit none
+
+    logical :: all_passed
+
+    print *, '=== direct session iso_c_binding compiler test ==='
+
+    all_passed = .true.
+    if (.not. test_use_iso_c_binding_alone_compiles()) all_passed = .false.
+    if (.not. test_use_iso_c_binding_intrinsic_form()) all_passed = .false.
+    if (.not. test_c_int_kind_used_in_declaration()) all_passed = .false.
+
+    if (.not. all_passed) stop 1
+    print *, 'PASS: iso_c_binding kind constants lower through direct LIRIC'
+
+contains
+
+    logical function test_use_iso_c_binding_alone_compiles()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  use iso_c_binding'//new_line('a')// &
+            '  integer(c_int32_t) :: x'//new_line('a')// &
+            '  x = 7'//new_line('a')// &
+            '  stop x'//new_line('a')// &
+            'end program main'
+
+        test_use_iso_c_binding_alone_compiles = expect_exit_status( &
+            source, 7, '/tmp/ffc_session_iso_c_c_int32_test')
+    end function test_use_iso_c_binding_alone_compiles
+
+    logical function test_use_iso_c_binding_intrinsic_form()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  use, intrinsic :: iso_c_binding'//new_line('a')// &
+            '  integer(c_int32_t) :: x'//new_line('a')// &
+            '  x = 7'//new_line('a')// &
+            '  stop x'//new_line('a')// &
+            'end program main'
+
+        test_use_iso_c_binding_intrinsic_form = expect_exit_status( &
+            source, 7, '/tmp/ffc_session_iso_c_intrinsic_test')
+    end function test_use_iso_c_binding_intrinsic_form
+
+    logical function test_c_int_kind_used_in_declaration()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  use iso_c_binding'//new_line('a')// &
+            '  integer(c_int) :: x'//new_line('a')// &
+            '  x = 5'//new_line('a')// &
+            '  stop x'//new_line('a')// &
+            'end program main'
+
+        test_c_int_kind_used_in_declaration = expect_exit_status( &
+            source, 5, '/tmp/ffc_session_iso_c_c_int_test')
+    end function test_c_int_kind_used_in_declaration
+
+end program test_session_iso_c_binding_compiler
