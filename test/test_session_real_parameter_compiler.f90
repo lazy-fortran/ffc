bb52391c524fa08b35ba8b8b5fb4787c6fcea3ad
Christopher Albert <albert@tugraz.at>
2026-07-31 19:57:56 +0200

    [old-parameter-01] collect old-style PARAMETER bindings (#504)
    Bind an untyped old-style `parameter (name = expr)` entity, which
    FortFront emits as a free-standing declaration outside the scope body,
    using default implicit typing so later specification expressions such as
    array bounds fold against the constant.
    
    Refs #355

--- test/test_session_real_parameter_compiler.f90
+++ test/test_session_real_parameter_compiler.f90
@@ -1,5 +1,5 @@
 program test_session_real_parameter_compiler
-    use ffc_test_support, only: expect_output
+    use ffc_test_support, only: expect_error_contains, expect_output
     implicit none
     character(len=1), parameter :: nl = new_line('a')
 
@@ -27,6 +27,32 @@ program test_session_real_parameter_compiler
         '   1.50000000    '//nl// &
         ' branch'//nl, &
         '/tmp/ffc_session_real_parameter_test')) stop 1
+
+    ! Old-style PARAMETER statement: the constant takes its implicit type
+    ! (i-n is integer) and folds into a later array bound.
+    if (.not. expect_output( &
+        'program main'//nl// &
+        '  parameter (ialen = 42)'//nl// &
+        '  integer :: myarray(ialen)'//nl// &
+        '  myarray = 0'//nl// &
+        '  print *, size(myarray)'//nl// &
+        '  print *, ialen'//nl// &
+        'end program main', &
+        '          42'//nl// &
+        '          42'//nl, &
+        '/tmp/ffc_session_old_parameter_test')) stop 1
+
+    ! A PARAMETER initializer that is not a constant expression is rejected.
+    if (.not. expect_error_contains( &
+        'program main'//nl// &
+        '  integer :: k'//nl// &
+        '  k = 3'//nl// &
+        '  parameter (ialen = k)'//nl// &
+        '  integer :: myarray(ialen)'//nl// &
+        '  print *, size(myarray)'//nl// &
+        'end program main', &
+        'compile-time integer parameter was not declared', &
+        '/tmp/ffc_session_old_parameter_nonconst_test')) stop 1
 
     print *, 'PASS: real/logical/character parameters lower through LIRIC'
 end program test_session_real_parameter_compiler
