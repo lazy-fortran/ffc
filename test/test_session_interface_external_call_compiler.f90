00d4240d84243ec6f6d1589ea5c7af970139d700
Christopher Albert <albert@tugraz.at>
2026-07-31 22:14:27 +0200

    [interface-body-01] cover unused interface blocks in procedure bodies (refs #357)

--- test/test_session_interface_external_call_compiler.f90
+++ test/test_session_interface_external_call_compiler.f90
@@ -42,5 +42,39 @@ program test_session_interface_external_call_compiler
         'end program', 0, &
         '/tmp/ffc_session_interface_external_call_test')) stop 1
 
+    ! An INTERFACE block inside the specification part of a module procedure
+    ! only declares signatures; it must not terminate the procedure. The
+    ! declarations and executable statements that follow it stay part of the
+    ! body, and the program unit after the module is still compiled.
+    if (.not. expect_exit_status( &
+        'module bench_mod'//new_line('a')// &
+        'contains'//new_line('a')// &
+        '    subroutine run(x)'//new_line('a')// &
+        '        interface'//new_line('a')// &
+        '            integer function f()'//new_line('a')// &
+        '            end function f'//new_line('a')// &
+        '        end interface'//new_line('a')// &
+        '        integer :: x'//new_line('a')// &
+        '        if (x /= 1) error stop'//new_line('a')// &
+        '        print *, x'//new_line('a')// &
+        '    end subroutine run'//new_line('a')// &
+        '    integer function twice(y)'//new_line('a')// &
+        '        interface'//new_line('a')// &
+        '            subroutine sub(a)'//new_line('a')// &
+        '                integer, intent(in) :: a'//new_line('a')// &
+        '            end subroutine sub'//new_line('a')// &
+        '        end interface'//new_line('a')// &
+        '        integer, intent(in) :: y'//new_line('a')// &
+        '        twice = 2 * y'//new_line('a')// &
+        '    end function twice'//new_line('a')// &
+        'end module bench_mod'//new_line('a')// &
+        'program p'//new_line('a')// &
+        '    use bench_mod'//new_line('a')// &
+        '    implicit none'//new_line('a')// &
+        '    call run(1)'//new_line('a')// &
+        '    if (twice(3) /= 6) error stop'//new_line('a')// &
+        'end program p', 0, &
+        '/tmp/ffc_session_interface_unused_body_test')) stop 1
+
     print *, 'PASS: external functions called through explicit interfaces'
 end program test_session_interface_external_call_compiler
