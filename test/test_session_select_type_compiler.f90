4228f0c8f78231aa717fc723f6afa1345ff6d0a7
Christopher Albert <albert@tugraz.at>
2026-08-01 17:40:40 +0200

    Support host-associated polymorphic allocatables

--- test/test_session_select_type_compiler.f90
+++ test/test_session_select_type_compiler.f90
@@ -1,5 +1,5 @@
 program test_session_select_type_compiler
-    use ffc_test_support, only: expect_exit_status
+    use ffc_test_support, only: expect_exit_status, expect_output
     implicit none
 
     logical :: all_passed
@@ -13,6 +13,7 @@ program test_session_select_type_compiler
     if (.not. test_select_type_two_arms_first_matches()) all_passed = .false.
     if (.not. test_select_type_two_arms_second_matches()) all_passed = .false.
     if (.not. test_select_type_class_default_matches_neither()) all_passed = .false.
+    if (.not. test_host_select_type_class_is()) all_passed = .false.
 
     if (.not. all_passed) stop 1
     print *, 'PASS: class(*) and select type lower through direct LIRIC'
@@ -92,6 +93,30 @@ contains
         test_select_type_class_default_matches_neither = expect_exit_status( &
             two_arm_source('.true.'), 9, '/tmp/ffc_session_st_default_test')
     end function test_select_type_class_default_matches_neither
+
+    logical function test_host_select_type_class_is()
+        ! A contained procedure must see the host class descriptor after the
+        ! host program allocates it; CLASS IS then selects the declared type.
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  type t'//new_line('a')// &
+            '  end type t'//new_line('a')// &
+            '  class(t), allocatable :: x'//new_line('a')// &
+            '  allocate (t :: x)'//new_line('a')// &
+            '  call s'//new_line('a')// &
+            'contains'//new_line('a')// &
+            '  subroutine s'//new_line('a')// &
+            '    select type (x)'//new_line('a')// &
+            '    class is (t)'//new_line('a')// &
+            "      print *, 'class is t'"//new_line('a')// &
+            '    end select'//new_line('a')// &
+            '  end subroutine s'//new_line('a')// &
+            'end program main'
+
+        test_host_select_type_class_is = expect_output( &
+            source, ' class is t'//new_line('a'), &
+            '/tmp/ffc_session_host_class_select')
+    end function test_host_select_type_class_is
 
     function two_arm_source(actual) result(source)
         ! integer arm -> 1, real arm -> 2, class default -> 9.
