f33dde7bafe1b76cdc67f1b003d91836325d4350
Christopher Albert <albert@tugraz.at>
2026-08-01 07:56:08 +0200

    [reshape-01] lower RESHAPE with order= permutation (#450) (#610)
    * [reshape-01] lower RESHAPE order= permutation and reject negative extents (refs #450)
    
    * [reshape-01] document reshape order= support (refs #450)

--- test/test_session_reshape_compiler.f90
+++ test/test_session_reshape_compiler.f90
@@ -1,5 +1,5 @@
 program test_session_reshape_compiler
-    use ffc_test_support, only: expect_output
+    use ffc_test_support, only: expect_output, expect_error_contains
     implicit none
 
     logical :: all_passed
@@ -13,6 +13,13 @@ program test_session_reshape_compiler
     if (.not. test_zero_sized_expression_source()) all_passed = .false.
     if (.not. test_zero_sized_assignment()) all_passed = .false.
     if (.not. test_pad_from_zero_sized_source()) all_passed = .false.
+    if (.not. test_order_permutation_rank2()) all_passed = .false.
+    if (.not. test_order_identity_rank2()) all_passed = .false.
+    if (.not. test_rank2_source_to_rank1()) all_passed = .false.
+    if (.not. test_pad_cycling_rank2()) all_passed = .false.
+    if (.not. test_invalid_order_is_rejected()) all_passed = .false.
+    if (.not. test_negative_extent_is_rejected()) all_passed = .false.
+    if (.not. test_short_source_without_pad_is_rejected()) all_passed = .false.
 
     if (.not. all_passed) stop 1
     print *, 'PASS: reshape lowers through direct LIRIC session'
@@ -128,5 +135,135 @@ contains
             '           8'//new_line('a'), &
             '/tmp/ffc_session_reshape_pad_test')
     end function test_pad_from_zero_sized_source
+
+    ! order=[2, 1] fills the result with the second dimension varying fastest,
+    ! so m(i, j) reads 1..6 row by row (verified against gfortran).
+    logical function test_order_permutation_rank2()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: src(6)'//new_line('a')// &
+            '  integer :: m(3, 2)'//new_line('a')// &
+            '  integer :: i, j'//new_line('a')// &
+            '  src = [1, 2, 3, 4, 5, 6]'//new_line('a')// &
+            '  m = reshape(src, [3, 2], order=[2, 1])'//new_line('a')// &
+            '  do i = 1, 3'//new_line('a')// &
+            '     do j = 1, 2'//new_line('a')// &
+            '        print *, m(i, j)'//new_line('a')// &
+            '     end do'//new_line('a')// &
+            '  end do'//new_line('a')// &
+            'end program main'
+        test_order_permutation_rank2 = expect_output( &
+            source, '           1'//new_line('a')// &
+            '           2'//new_line('a')// &
+            '           3'//new_line('a')// &
+            '           4'//new_line('a')// &
+            '           5'//new_line('a')// &
+            '           6'//new_line('a'), &
+            '/tmp/ffc_session_reshape_order_test')
+    end function test_order_permutation_rank2
+
+    ! The identity order is equivalent to omitting order entirely.
+    logical function test_order_identity_rank2()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: m(2, 2)'//new_line('a')// &
+            '  m = reshape([1, 2, 3, 4], [2, 2], order=[1, 2])'//new_line('a')// &
+            '  print *, m(1, 1)'//new_line('a')// &
+            '  print *, m(2, 1)'//new_line('a')// &
+            '  print *, m(1, 2)'//new_line('a')// &
+            '  print *, m(2, 2)'//new_line('a')// &
+            'end program main'
+        test_order_identity_rank2 = expect_output( &
+            source, '           1'//new_line('a')// &
+            '           2'//new_line('a')// &
+            '           3'//new_line('a')// &
+            '           4'//new_line('a'), &
+            '/tmp/ffc_session_reshape_order_identity_test')
+    end function test_order_identity_rank2
+
+    ! Flattening a rank-2 array back to rank-1 keeps column-major order.
+    logical function test_rank2_source_to_rank1()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: src(2, 3)'//new_line('a')// &
+            '  integer :: v(6)'//new_line('a')// &
+            '  integer :: i'//new_line('a')// &
+            '  src = reshape([1, 2, 3, 4, 5, 6], [2, 3])'//new_line('a')// &
+            '  v = reshape(src, [6])'//new_line('a')// &
+            '  do i = 1, 6'//new_line('a')// &
+            '     print *, v(i)'//new_line('a')// &
+            '  end do'//new_line('a')// &
+            'end program main'
+        test_rank2_source_to_rank1 = expect_output( &
+            source, '           1'//new_line('a')// &
+            '           2'//new_line('a')// &
+            '           3'//new_line('a')// &
+            '           4'//new_line('a')// &
+            '           5'//new_line('a')// &
+            '           6'//new_line('a'), &
+            '/tmp/ffc_session_reshape_flatten_test')
+    end function test_rank2_source_to_rank1
+
+    ! pad supplies the elements past the source, cycling in element order.
+    logical function test_pad_cycling_rank2()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: m(2, 3)'//new_line('a')// &
+            '  integer :: i, j'//new_line('a')// &
+            '  m = reshape([1, 2, 3, 4], [2, 3], pad=[9, 8])'//new_line('a')// &
+            '  do j = 1, 3'//new_line('a')// &
+            '     do i = 1, 2'//new_line('a')// &
+            '        print *, m(i, j)'//new_line('a')// &
+            '     end do'//new_line('a')// &
+            '  end do'//new_line('a')// &
+            'end program main'
+        test_pad_cycling_rank2 = expect_output( &
+            source, '           1'//new_line('a')// &
+            '           2'//new_line('a')// &
+            '           3'//new_line('a')// &
+            '           4'//new_line('a')// &
+            '           9'//new_line('a')// &
+            '           8'//new_line('a'), &
+            '/tmp/ffc_session_reshape_padcycle_test')
+    end function test_pad_cycling_rank2
+
+    ! order must be a permutation of the target dimensions.
+    logical function test_invalid_order_is_rejected()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: m(2, 2)'//new_line('a')// &
+            '  m = reshape([1, 2, 3, 4], [2, 2], order=[1, 1])'//new_line('a')// &
+            '  print *, m(1, 1)'//new_line('a')// &
+            'end program main'
+        test_invalid_order_is_rejected = expect_error_contains( &
+            source, 'reshape order must be a permutation', &
+            '/tmp/ffc_session_reshape_bad_order_test')
+    end function test_invalid_order_is_rejected
+
+    ! A negative extent in shape is not a valid array shape.
+    logical function test_negative_extent_is_rejected()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: m(2, 3)'//new_line('a')// &
+            '  m = reshape([1, 2, 3, 4, 5, 6], [-2, 3])'//new_line('a')// &
+            '  print *, m(1, 1)'//new_line('a')// &
+            'end program main'
+        test_negative_extent_is_rejected = expect_error_contains( &
+            source, 'reshape shape extents must not be negative', &
+            '/tmp/ffc_session_reshape_negative_extent_test')
+    end function test_negative_extent_is_rejected
+
+    ! Too few source elements and no pad is a size error.
+    logical function test_short_source_without_pad_is_rejected()
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer :: m(2, 3)'//new_line('a')// &
+            '  m = reshape([1, 2, 3], [2, 3])'//new_line('a')// &
+            '  print *, m(1, 1)'//new_line('a')// &
+            'end program main'
+        test_short_source_without_pad_is_rejected = expect_error_contains( &
+            source, 'reshape requires source and target arrays of equal size', &
+            '/tmp/ffc_session_reshape_short_source_test')
+    end function test_short_source_without_pad_is_rejected
 
 end program test_session_reshape_compiler
