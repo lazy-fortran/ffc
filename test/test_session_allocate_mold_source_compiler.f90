b9789ab0d56726ebf53444b3513f1cb43aa8b3c9
Christopher Albert <albert@tugraz.at>
2026-08-25 02:52:51 +0200

    Support rank-4 mold and source allocation

--- test/test_session_allocate_mold_source_compiler.f90
+++ test/test_session_allocate_mold_source_compiler.f90
@@ -16,6 +16,7 @@ program test_session_allocate_mold_source
     if (.not. test_rank2_mold_copies_shape()) all_passed = .false.
     if (.not. test_rank2_source_copies_values()) all_passed = .false.
     if (.not. test_rank3_mold_source_matches_gfortran()) all_passed = .false.
+    if (.not. test_rank4_mold_source_matches_gfortran()) all_passed = .false.
     if (.not. test_issue_2820_roundtrip()) all_passed = .false.
 
     if (.not. all_passed) stop 1
@@ -116,6 +117,46 @@ contains
         test_rank3_mold_source_matches_gfortran = expect_output_matches_gfortran( &
             source, 'alloc_mold_source_rank3')
     end function test_rank3_mold_source_matches_gfortran
+
+    logical function test_rank4_mold_source_matches_gfortran()
+        ! Runtime descriptor extents and column-major values exercise all four
+        ! dimensions of MOLD= and SOURCE=.
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer, allocatable :: a(:,:,:,:), b(:,:,:,:), c(:,:,:,:)'// &
+            new_line('a')// &
+            '  integer :: n, m, p, q, i, j, k, l, value'//new_line('a')// &
+            '  n = 2'//new_line('a')// &
+            '  m = 2'//new_line('a')// &
+            '  p = 2'//new_line('a')// &
+            '  q = 2'//new_line('a')// &
+            '  allocate(a(n,m,p,q))'//new_line('a')// &
+            '  value = 101'//new_line('a')// &
+            '  do l = 1, q'//new_line('a')// &
+            '    do k = 1, p'//new_line('a')// &
+            '      do j = 1, m'//new_line('a')// &
+            '        do i = 1, n'//new_line('a')// &
+            '          a(i,j,k,l) = value'//new_line('a')// &
+            '          value = value + 1'//new_line('a')// &
+            '        end do'//new_line('a')// &
+            '      end do'//new_line('a')// &
+            '    end do'//new_line('a')// &
+            '  end do'//new_line('a')// &
+            '  allocate(b, mold=a)'//new_line('a')// &
+            '  allocate(c, source=a)'//new_line('a')// &
+            '  print *, size(b,1), size(b,2), size(b,3), size(b,4)'// &
+            new_line('a')// &
+            '  print *, c(1,1,1,1), c(2,1,1,1), c(1,2,1,1), '// &
+            'c(2,2,1,1), c(1,1,2,1), c(2,1,2,1), c(1,2,2,1), '// &
+            'c(2,2,2,1)'//new_line('a')// &
+            '  print *, c(1,1,1,2), c(2,1,1,2), c(1,2,1,2), '// &
+            'c(2,2,1,2), c(1,1,2,2), c(2,1,2,2), c(1,2,2,2), '// &
+            'c(2,2,2,2)'//new_line('a')// &
+            'end program main'
+
+        test_rank4_mold_source_matches_gfortran = expect_output_matches_gfortran( &
+            source, 'alloc_mold_source_rank4')
+    end function test_rank4_mold_source_matches_gfortran
 
     logical function test_issue_2820_roundtrip()
         ! The corpus program: print size(b) then c (all ones).
