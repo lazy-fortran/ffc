3856ce1b38260eec641dc5eb0d465815bf04b494
Christopher Albert <albert@tugraz.at>
2026-08-25 02:45:00 +0200

    Extend allocate mold source to rank three

--- test/test_session_allocate_mold_source_compiler.f90
+++ test/test_session_allocate_mold_source_compiler.f90
@@ -1,8 +1,9 @@
 program test_session_allocate_mold_source
-    ! ALLOCATE with MOLD= and SOURCE= on 1-D integer allocatables (#2820).
+    ! ALLOCATE with MOLD= and SOURCE= on intrinsic allocatable arrays (#2820).
     ! MOLD copies the source's shape (contents are undefined). SOURCE copies
     ! both shape and element values.
-    use ffc_test_support, only: expect_exit_status, expect_output
+    use ffc_test_support, only: expect_exit_status, expect_output, &
+        expect_output_matches_gfortran
     implicit none
 
     logical :: all_passed
@@ -14,6 +15,7 @@ program test_session_allocate_mold_source
     if (.not. test_source_copies_values()) all_passed = .false.
     if (.not. test_rank2_mold_copies_shape()) all_passed = .false.
     if (.not. test_rank2_source_copies_values()) all_passed = .false.
+    if (.not. test_rank3_mold_source_matches_gfortran()) all_passed = .false.
     if (.not. test_issue_2820_roundtrip()) all_passed = .false.
 
     if (.not. all_passed) stop 1
@@ -79,6 +81,41 @@ contains
         test_rank2_source_copies_values = expect_exit_status( &
             source, 129, '/tmp/ffc_alloc_source_rank2_values')
     end function test_rank2_source_copies_values
+
+    logical function test_rank3_mold_source_matches_gfortran()
+        ! Runtime descriptor extents and the values distinguish column-major
+        ! storage from a mistaken row-major copy.
+        character(len=*), parameter :: source = &
+            'program main'//new_line('a')// &
+            '  integer, allocatable :: a(:,:,:), b(:,:,:), c(:,:,:)'//new_line('a')// &
+            '  integer :: n, m, p'//new_line('a')// &
+            '  n = 2'//new_line('a')// &
+            '  m = 2'//new_line('a')// &
+            '  p = 3'//new_line('a')// &
+            '  allocate(a(n,m,p))'//new_line('a')// &
+            '  a(1,1,1) = 11'//new_line('a')// &
+            '  a(2,1,1) = 12'//new_line('a')// &
+            '  a(1,2,1) = 21'//new_line('a')// &
+            '  a(2,2,1) = 22'//new_line('a')// &
+            '  a(1,1,2) = 31'//new_line('a')// &
+            '  a(2,1,2) = 32'//new_line('a')// &
+            '  a(1,2,2) = 41'//new_line('a')// &
+            '  a(2,2,2) = 42'//new_line('a')// &
+            '  a(1,1,3) = 51'//new_line('a')// &
+            '  a(2,1,3) = 52'//new_line('a')// &
+            '  a(1,2,3) = 61'//new_line('a')// &
+            '  a(2,2,3) = 62'//new_line('a')// &
+            '  allocate(b, mold=a)'//new_line('a')// &
+            '  allocate(c, source=a)'//new_line('a')// &
+            '  print *, size(b,1), size(b,2), size(b,3)'//new_line('a')// &
+            '  print *, c(1,1,1), c(2,1,1), c(1,2,1), c(2,2,1), '// &
+            'c(1,1,2), c(2,1,2), c(1,2,2), c(2,2,2), '// &
+            'c(1,1,3), c(2,1,3), c(1,2,3), c(2,2,3)'//new_line('a')// &
+            'end program main'
+
+        test_rank3_mold_source_matches_gfortran = expect_output_matches_gfortran( &
+            source, 'alloc_mold_source_rank3')
+    end function test_rank3_mold_source_matches_gfortran
 
     logical function test_issue_2820_roundtrip()
         ! The corpus program: print size(b) then c (all ones).
